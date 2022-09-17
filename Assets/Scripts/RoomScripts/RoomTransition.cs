d480ace6491a0360446d8085b5e936e2d67cd061
AlmityTuhm <driggsthomas@gmail.com>
2022-09-17 10:31:56 -0600

    There is a room transition now

--- Assets/Scripts/RoomScripts/RoomTransition.cs
+++ Assets/Scripts/RoomScripts/RoomTransition.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class RoomTransition : MonoBehaviour
 {
+    [SerializeField] GameObject playerSpawnSpace = null;
+    [SerializeField] float transiftionTime = 2;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +17,26 @@ public class RoomTransition : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.TryGetComponent<PlayerController>(out PlayerController player))
+        {
+            return;
+        }
+
+        player.gameObject.SetActive(false);
+
+        StartCoroutine(RoomChangeTime(player));
+
+    }
+
+    private IEnumerator RoomChangeTime(PlayerController player)
+    {
+        yield return new WaitForSeconds(transiftionTime);
+
+        player.gameObject.SetActive(true);
+        player.transform.position = playerSpawnSpace.transform.position;
     }
 }

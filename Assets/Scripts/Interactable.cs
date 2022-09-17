ecb629914501446aedc07a97770e21e9f0bbc34e
Enoch-Nielsen <93846909+Enoch-Nielsen@users.noreply.github.com>
2022-09-17 10:35:48 -0600

    Updating Main, added bases for object interaction and a game manager.

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -1,18 +1,51 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Interactable : MonoBehaviour
 {
+    // Outline Variables
+    [SerializeField] private Material outline;
+    [SerializeField] [Range(0.0F, 1.0F)] private float outlineAlpha;
+
+    // Interactable Variables
+    [SerializeField] private GameManager _gameManager;
+    [SerializeField]
+    
+    public bool requiresKey;
+    public bool givesItem;
+    
+    public bool hasInterface;
+    public bool isDoor;
+
+    // Conditional Variables
+    public GameObject interactiveInterface;
+    public GameManager.Item key;
+    public GameManager.Item item;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        outline.SetFloat("_Alpha", 0);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider collider)
     {
-        
+        if (collider.CompareTag("Player"))
+        {
+            outline.SetFloat("_Alpha", outlineAlpha);
+            _gameManager.playerCanInteract = true;
+            collider.GetComponent<PlayerController>().currentInteractable = this;
+        }
+    }
+
+    private void OnTriggerExit(Collider collider)
+    {
+        if (collider.CompareTag("Player"))
+        {
+            outline.SetFloat("_Alpha", 0);
+            _gameManager.playerCanInteract = true;
+        }
     }
 }
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -8,14 +8,15 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float speed = 5;
 
     //Player public Variables
-    public bool canInteract = false;
 
     //Player Specific Variables
+    [SerializeField] private GameManager _gameManager;
+    public Interactable currentInteractable;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
@@ -24,7 +25,42 @@ public class PlayerController : MonoBehaviour
         float horizontalMovement = Input.GetAxis("Horizontal");
         float verticalMovement = Input.GetAxis("Vertical");
 
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalMovement);
-        transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalMovement);
+        if (_gameManager.gameState == GameManager.GameState.Moving)
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalMovement);
+            transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalMovement);
+        }
+        
+        // Check Player Interact
+        if (Input.GetKeyDown(KeyCode.E) && _gameManager.playerCanInteract)
+        {
+            if (currentInteractable.requiresKey)
+            {
+                bool playerHasKey = false;
+
+                foreach (var item in _gameManager.playerInventory)
+                {
+                    if (item == currentInteractable.key)
+                    {
+                        playerHasKey = true;
+                        _gameManager.playerInventory.Remove(item);
+                    }
+                }
+
+                if (!playerHasKey)
+                    return;
+            }
+
+            if (currentInteractable.givesItem)
+            {
+                _gameManager.playerInventory.Add(currentInteractable.item);
+            }
+            
+            if (currentInteractable.hasInterface)
+            {
+                _gameManager.gameState = GameManager.GameState.Interacting;
+                currentInteractable.interactiveInterface.SetActive(true);
+            }
+        }
     }
 }

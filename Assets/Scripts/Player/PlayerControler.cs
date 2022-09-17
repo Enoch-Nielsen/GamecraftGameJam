a2babd336e1b1cafb78c2db9c3a7d0ef9f977126
AlmityTuhm <driggsthomas@gmail.com>
2022-09-16 22:10:50 -0600

    PlayerMovement

---
+++ Assets/Scripts/Player/PlayerControler.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+    //Player edited Variables
+    [SerializeField] private float speed = 5;
+
+    //Player public Variables
+    public bool canInteract = false;
+
+    //Player Specific Variables
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalMovement = Input.GetAxis("Horizontal");
+        float verticalMovement = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalMovement);
+        transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalMovement);
+    }
+}

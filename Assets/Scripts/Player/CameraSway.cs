919712cbff8a03e31c595d18e09d245f7ab484a9
AlmityTuhm <driggsthomas@gmail.com>
2022-09-17 14:46:47 -0600

    MenuChanges

--- Assets/Scripts/Player/CameraSway.cs
+++ Assets/Scripts/Player/CameraSway.cs
@@ -6,26 +6,28 @@ public class CameraSway : MonoBehaviour
 {
     [SerializeField] float swaySpeed = 1;
     [SerializeField] Transform cameraTransform = null;
+    [SerializeField] float swayHold = 2;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Transform currentRotation = GetTransform();
-        if(currentRotation.transform.rotation.z < -0.05f)
-        {
-            swaySpeed *= -1;
-        }
-        else if(currentRotation.transform.rotation.z > 0.05f)
-        {
-            swaySpeed *= -1;
-        }
-            cameraTransform.Rotate(Vector3.forward * Time.deltaTime * swaySpeed);
 
+        Transform currentRotation = GetTransform();
+        cameraTransform.Rotate(Vector3.forward * Time.deltaTime * swaySpeed);
+        
+        if (currentRotation.transform.rotation.z < -0.05f)
+        {
+            swaySpeed *= -1;
+        }
+        else if (currentRotation.transform.rotation.z > 0.05f)
+        {
+            swaySpeed *= -1;
+        }
     }
 
     private Transform GetTransform()
@@ -34,4 +36,5 @@ public class CameraSway : MonoBehaviour
         //Debug.Log(cameraTransform.rotation.z);
         return cameraTransform;
     }
+
 }

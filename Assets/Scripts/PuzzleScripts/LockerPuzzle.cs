8dc7f88c0c2629634146d70737d30ad218d40ea0
Enoch-Nielsen <93846909+Enoch-Nielsen@users.noreply.github.com>
2022-09-18 09:28:16 -0600

    Made Locker Puzzle

---
+++ Assets/Scripts/PuzzleScripts/LockerPuzzle.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockerPuzzle : MonoBehaviour
+{
+    [SerializeField] private GameManager _gameManager;
+    public List<UIItem> lockerItems;
+    
+    private bool doDelay;
+    private float delayTimer = 1.0f;
+
+    private void Update()
+    {
+        if (doDelay)
+        {
+            delayTimer -= Time.deltaTime;
+        }
+        
+        if (lockerItems.Count == 0)
+        {
+            doDelay = true;
+        
+            if (delayTimer <= 0)
+            {
+                _gameManager.playerInventory.Add(GameManager.Item.RoomKey);
+                _gameManager.gameState = GameManager.GameState.Idle;
+                _gameManager.playerMessage.SendMessage("You obtained the Room Key!");
+                _gameManager.currentPlayerInteractable.canInteract = false;
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}

6456777d7e44fc7940d7979e4f4e90dd4e922f97
Enoch-Nielsen <93846909+Enoch-Nielsen@users.noreply.github.com>
2022-09-18 14:42:36 -0600

    Started Crate

--- Assets/Scripts/PuzzleScripts/Crate.cs
+++ Assets/Scripts/PuzzleScripts/Crate.cs
@@ -6,118 +6,61 @@ using UnityEngine.UI;
 
 public class Crate : MonoBehaviour
 {
-    [SerializeField] private CratePuzzle cratePuzzle;
+    public CratePuzzle cratePuzzle;
     
-    // 0 : Nothing, 1 : White, 2 : Black.
-    [Tooltip("0 : Nothing, 1 : White, 2 : Black.")]
-    public int crateId;
-    
-    [SerializeField] private bool leftValid, rightValid, upValid, downValid;
-    [SerializeField] private bool leftCheck, rightCheck, upCheck, downCheck;
+    public bool canMove;
+    public bool isMoving;
 
-    public int index;
+    public float speed;
+    public float moveTime, moveTimer;
+
     public Image selfImage;
-    public bool doHighlight;
-    public bool canSwap;
 
+    private Vector2 _direction;
+    
     private void Start()
     {
         selfImage.color = Color.white;
+        moveTimer = moveTime;
     }
 
     private void Update()
     {
-        if (doHighlight)
+        if (moveTimer <= moveTime)
         {
-            selfImage.color = Color.yellow;
+            transform.Translate(_direction * speed * Time.deltaTime);
         }
         else
         {
-            selfImage.color = Color.white;
+            isMoving = false;
+            _direction = new Vector2(0, 0);
         }
-        
-        // Modify Position Here.
-        Vector3 pipePosition = new Vector3(cratePuzzle.cratePositionList[index].x,
-            cratePuzzle.cratePositionList[index].y, 0);
-        
-        gameObject.GetComponent<RectTransform>().localPosition = pipePosition;
     }
-
-    public void OnPlayerClick()
+    
+    // 0 Left, 1 Right, 2 Up, 3 Down.
+    public void Move(int direction)
     {
-        Debug.Log(this);
-        if (crateId == 0)
+        if (isMoving) return;
+        
+        float moveTimer = 0;
+
+        switch (direction)
         {
-            if (canSwap)
-            {
-                canSwap = false;
-                doHighlight = false;
-
-                // Swap Current ID's
-                int tempId = crateId;
-
-                cratePuzzle.currentPipeLayout[index] = cratePuzzle.currentPipeLayout[cratePuzzle.selectedPipe.index];
-                cratePuzzle.currentPipeLayout[cratePuzzle.selectedPipe.index] = tempId;
-
-                int tempIndex = index;
-
-                index = cratePuzzle.selectedPipe.index;
-                cratePuzzle.selectedPipe.index = tempIndex;
-
-                Pipe tempPipe = cratePuzzle.crateList[index];
-
-                cratePuzzle.crateList[index] = cratePuzzle.crateList[cratePuzzle.selectedPipe.index];
-                cratePuzzle.crateList[cratePuzzle.selectedPipe.index] = tempPipe;
-                
-                for (int i = 0; i < 25; i++)
-                {
-                    cratePuzzle.crateList[i].doHighlight = false;
-                    cratePuzzle.crateList[i].canSwap = false;
-                }
-            }
+            case 0:
+                _direction = new Vector2(-1, 0);
+                break;
             
-            return;
-        }
-        
-        cratePuzzle.selectedPipe = this;
-        
-        for (int i = 0; i < 25; i++)
-        {
-            cratePuzzle.crateList[i].doHighlight = false;
-            cratePuzzle.crateList[i].canSwap = false;
-        }
-
-        leftCheck = index - 5 > -1;
-        rightCheck = index + 5 < 25;
-        upCheck = index - 1 > -1 && (index % 5 != 0);
-        downCheck = index + 1 < 25 && ((index + 1) % 5 != 0);
-
-        if (leftCheck)
-        {
-            leftValid = cratePuzzle.crateList[index - 5].pipeId == 0;
-            cratePuzzle.crateList[index - 5].doHighlight = leftValid;
-            cratePuzzle.crateList[index - 5].canSwap = leftValid;
-        }
-
-        if (rightCheck)
-        {
-            rightValid = cratePuzzle.crateList[index + 3].pipeId == 0;
-            cratePuzzle.crateList[index + 5].doHighlight = rightValid;
-            cratePuzzle.crateList[index + 5].canSwap = rightValid;
-        }
-
-        if (upCheck)
-        {
-            upValid = cratePuzzle.crateList[index - 5].pipeId == 0;
-            cratePuzzle.crateList[index - 5].doHighlight = upValid;
-            cratePuzzle.crateList[index - 5].canSwap = upValid;
-        }
-
-        if (downCheck)
-        {
-            downValid = cratePuzzle.crateList[index + 5].pipeId == 0;
-            cratePuzzle.crateList[index + 5].doHighlight = downValid;
-            cratePuzzle.crateList[index + 5].canSwap = downValid;
+            case 1:
+                _direction = new Vector2(1, 0);
+                break;
+            
+            case 2:
+                _direction = new Vector2(0, 1);
+                break;
+            
+            case 3:
+                _direction = new Vector2(0, -1);
+                break;
         }
     }
 }
--- Assets/Scripts/PuzzleScripts/CratePuzzle.cs
+++ Assets/Scripts/PuzzleScripts/CratePuzzle.cs
@@ -1,37 +1,86 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class CratePuzzle : MonoBehaviour
 {
     [SerializeField] private GameManager gameManager;
     [SerializeField] private bool solved = true;
+    [SerializeField] private GameObject whiteCrate, blackCrate;
     
     // 0 : Nothing, 1 : White, 2 : Black.
-    public int[] currentPipeLayout = new int[] {0, 5, 1, 0, 1, 4, 4, 2, 3};
-    private int[] finalPipeLayout = new[] {0, 0, 1, 2, 5, 1, 4, 3, 4};
-    
 
-    public List<Pipe> crateList;
+    public List<int> initialCrateLayout;
+    public List<Crate> crateList;
     public List<Vector2> cratePositionList;
 
-    public Crate selectedPipe;
+    public Crate selectedCrate;
 
     private bool doDelay;
     private float delayTimer = 1.0f;
 
+    [SerializeField] private float crateSize;
+
+    private void Start()
+    {
+        int index = 0;
+
+        foreach (var position in cratePositionList)
+        {
+            
+        }
+        
+        foreach (var position in cratePositionList)
+        {
+            crateList.Add(Instantiate(initialCrateLayout[index] == 0 ? whiteCrate : blackCrate).GetComponent<Crate>());
+            crateList.Last().transform.position = position;
+            index++;
+        }
+
+        foreach (var crate in crateList)
+        {
+            crate.cratePuzzle = this;
+        }
+    }
+
     private void Update()
     {
         solved = true;
-        
-        //Checks if the puzzle has been solved.
-        for (int i = 0; i < 9; i++)
+
+        if (Input.GetKeyDown(KeyCode.A))
         {
-            if (currentPipeLayout[i] != finalPipeLayout[i])
+            foreach (var crate in crateList)
             {
-                solved = false;
-                Debug.Log(currentPipeLayout[i] + " | " + finalPipeLayout[i]);
+                if (!crate.isMoving)
+                {
+                    crate.Move(0);
+                }
+            }
+        }
+        
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            foreach (var crate in crateList)
+            {
+                crate.Move(1);
+            }
+        }
+        
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            foreach (var crate in crateList)
+            {
+                crate.Move(2);
+            }
+        }
+        
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            foreach (var crate in crateList)
+            {
+                crate.Move(3);
             }
         }
 
